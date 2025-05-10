af6ba45cab596339c5ef82097b9dc19839a9b405
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2025-05-10 16:01:01 +0530

    feat: rate limit url shorten endpoint

--- src/api/v1/routes/url.ts
+++ src/api/v1/routes/url.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 
 import { redirectShorten, shorten } from '../controllers/url'
+import shortenRateLimiter from '../../middlewares/ratelimiter/shortenRateLimiter'
 
 const router = express.Router()
 
-router.post('/shorten', shorten)
+router.post('/shorten', shortenRateLimiter, shorten)
 router.get('/:shortCode', redirectShorten)
 
 export default router
