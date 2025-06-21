85590f9aad8eb6add29f95e34f31eb87a716bc37
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2025-06-21 16:48:55 +0530

    add separate rate limiter for sign-in and sign-up

--- src/api/v1/routes/auth.ts
+++ src/api/v1/routes/auth.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 
-import authRateLimiter from '../../middlewares/ratelimiter/authRateLimiter'
+import {
+  signInRateLimiter,
+  signUpRateLimiter,
+} from '../../middlewares/ratelimiter/authRateLimiter'
 import { signIn, signOut, signUp } from '../controllers/auth'
 
 const authRouter = express.Router()
 
-authRouter.post('/signin', authRateLimiter, signIn)
+authRouter.post('/signin', signInRateLimiter, signIn)
 authRouter.post('/signout', signOut)
-authRouter.post('/signup', authRateLimiter, signUp)
+authRouter.post('/signup', signUpRateLimiter, signUp)
 
 export default authRouter
