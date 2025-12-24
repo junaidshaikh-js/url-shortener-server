d8e38ad1b834583ee9520366f4b6a3c1432d94fd
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2025-12-24 13:46:15 +0530

    feat: vercel cron

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -1,6 +1,6 @@
-import cron from 'node-cron'
+// import cron from 'node-cron'
 
-import numberOfShortCodes from './numberOfShortCodes'
+// import numberOfShortCodes from './numberOfShortCodes'
 
-// runs every 14 minutes
-cron.schedule('*/14 * * * *', numberOfShortCodes)
+// // runs every 14 minutes
+// cron.schedule('*/14 * * * *', numberOfShortCodes)
